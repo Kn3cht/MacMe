bf43f0e005a707994a560b037c809ba1b63a0fa3
Kn3cht <g.mahlknecht@gmail.com>
2020-10-01 14:35:46 +0200

    Add help prompt Add "me os" to get the current os version Add "me settings" to open the settings Add tutorial how to make the binary file accessible from all directories

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 This project contains a list of useful MacOS scripts that are united by the program "me"
 
 ## Installation
-To build the executable file execute 
-```make```
-in the root directory of this project.
+To build the executable file execute ``make`` in the root directory of this project.
+In order to make the program accessible from all directories it is necessary to copy the binary file to a location defined in the file ``~/.bash_profile.``. This can be done by executing the following command (use ``mv`` to move the file instead of creating a copy):
+```
+cp ~/MacMe/me ./bin
+```
--- main.c
+++ main.c
@@ -9,19 +9,31 @@
 #include <net/if.h>
 #include <arpa/inet.h>
 
+#define WELCOME "Welcome to MacMe!\n\nUse me -h to get an overview of all possible operation modes of me.\n"
+
 int main(int argc, char *argv[])
 {
-    
     if (argc == 1)
     { // no arguments
-        // show help
-        printf("Welcome to MacMe!\n\nUse me -h to get an overview of all possible operation modes of me.\n");
+        printf(WELCOME);
         return 0;
     } 
     else 
     {
         const char* program = argv[1];
-        if (strcmp(program, "ip") == 0)
+        if (strcmp(program, "-h") == 0) 
+        { // Show help prompt
+            const char* helpPrompt = "Usage: me [program | -h] [options...]\n\nPrograms include:\n \
+            \tip\t\t\t\tReturns the ip address of the network interface en0 (default).\n \
+            \tip [-i] <interface name>\tReturns the ip address of the specified interface.\n \
+            \tname\t\t\t\tReturns the user name of the currently logged in user.\n \
+            \tfinder\t\t\t\tOpens a finder window in the current directory.\n \
+            \tsettings\t\t\tOpens the settings window.\n \
+            \tos\t\t\t\tReturns the MacOS version.\n \
+            \n";
+            printf("%s", helpPrompt);
+        }
+        else if (strcmp(program, "ip") == 0)
         { // ip program
             char* interface = "en0"; // default wifi interface on macOS
             if (argc == 4 && strcmp(argv[2], "-i") == 0)
@@ -44,12 +56,17 @@ int main(int argc, char *argv[])
         { // print current user name
             system("echo $USER");
         }
-        else if (strcmp(program, "terminal"))
-        { // open terminal in current directory
+        else if (strcmp(program, "finder") == 0)
+        { // open finder in current directory
             system("open .");
         }
-        else if (strcmp(program, "")) {
+        else if (strcmp(program, "settings") == 0) 
+        { // open settings
             system("open \"x-apple.systempreferences:com.apple.preference\"");
+        }
+        else if (strcmp(program, "os") == 0) 
+        {
+            system("sw_vers -productVersion");
         }
     }
 }
